da51d5b49ca94c9f690b9e582462caddc4d11807
Miguel <sanchez.vc@hotmail.com>
2022-07-04 11:37:13 -0500

    Interface for the data, Showing the gifs in the UI

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root' //a nivel global de la aplicacion 
@@ -8,6 +9,8 @@ export class GifsService {
   
   private apiKey    : string = 'eUYOvSN1Y33k40ou9p4CD57IZ1FUeeJf' ; 
   private _historial: string[] = [];
+
+  public resultado : Gif[] = []; 
 
   get historial(){
     return [...this._historial];
@@ -28,9 +31,10 @@ export class GifsService {
          this._historial.slice(0,10);
     }
 
-    //forma de subscribirnos a un servicio, especificando el tipo de dato recibido para poder llamar sus aributos 
-    this.http.get('https://api.giphy.com/v1/gifs/trending?limit=10&api_key=eUYOvSN1Y33k40ou9p4CD57IZ1FUeeJf&q=brazil')
-    .subscribe((resp:any) => {
+    //forma de subscribirnos a un servicio, especificando el tipo de dato recibido (resp:any) para poder llamar sus aributos 
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?limit=10&api_key=${ this.apiKey }&q=${ query }`) //interpolacion para mandar el valor
+    .subscribe( ( resp ) => {
+      this.resultado = resp.data;
       console.log(resp.data);
     })
     
