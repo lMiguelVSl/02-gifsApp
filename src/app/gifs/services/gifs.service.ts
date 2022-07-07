b76b8dc8eac83e2d55e73d9caee08cf475dfacf5
Miguel <sanchez.vc@hotmail.com>
2022-07-06 21:42:06 -0500

    Configuring local storage for the historial

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -16,23 +16,33 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient ){} //importamos para poder hacer las peticiones http
+  constructor( private http: HttpClient ){ //importamos para poder hacer las peticiones http
 
-  buscarGifs( query : string){
+    if ( localStorage.getItem( 'Historial' ) ) {
+      
+      this._historial = JSON.parse( localStorage.getItem( 'Historial' )! ); //convertimos un objeto a un string y con ( ! ) le decimos que ignore que puede ser null 
+
+    }
+      
+  } 
+
+  buscarGifs( query : string ){  //recibimos el valor del input desde Busqueda.component
    
-    query = query.trim().toLowerCase();
+    query = query.trim().toLowerCase(); //convertir en minusculas 
 
-    if(this._historial.includes(query)) {
+    if(this._historial.includes(query)) { //si en el array existe el nombre 
       alert('This value already exists');
      }
 
     if (!this._historial.includes(query)) {
-         this._historial.unshift(query);
-         this._historial.slice(0,10);
+         this._historial.unshift(query); //guardar al inicio del array 
+         this._historial.slice(0,10); 
+
+         localStorage.setItem( 'Historial', JSON.stringify( this._historial ) );
     }
 
     //forma de subscribirnos a un servicio, especificando el tipo de dato recibido (resp:any) para poder llamar sus aributos 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?limit=10&api_key=${ this.apiKey }&q=${ query }`) //interpolacion para mandar el valor
+    this.http.get<SearchGifsResponse>( `https://api.giphy.com/v1/gifs/search?limit=10&api_key=${ this.apiKey }&q=${ query }` ) //interpolacion para mandar el valor
     .subscribe( ( resp ) => {
       this.resultado = resp.data;
       console.log(resp.data);
